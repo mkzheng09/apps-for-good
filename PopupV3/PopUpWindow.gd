3641d86fe6c5e12da5e223cb7760a097f4e372eb
liammmorrison <jlmorrison623@gmail.com>
2022-04-12 13:10:12 -0400

    Commit Popup V2

--- PopupV3/PopUpWindow.gd
+++ PopupV3/PopUpWindow.gd
@@ -2,7 +2,16 @@ extends Control
 
 onready var popup = get_node("WindowDialog")
 onready var help = get_node("Help")
+onready var tips = get_node("Tips")
+onready var suggestions = get_node("Suggestions")
+onready var popup2 = get_node("WindowDialog2")
+onready var popup3 = get_node("WindowDialog3")
 
 func _on_Help_button_up():
 	popup.visible=true
 
+func _on_Tips_button_up():
+	popup2.visable=true
+
+func _on_Suggestions_button_up():
+	popup3.visable=true
