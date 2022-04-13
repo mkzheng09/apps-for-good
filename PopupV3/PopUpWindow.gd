d97b8308812810bc211568eea2e97bc33f3fea4a
liammmorrison <jlmorrison623@gmail.com>
2022-04-13 13:18:09 -0400

    Commit 4/13
    Just to see what happens

--- PopupV3/PopUpWindow.gd
+++ PopupV3/PopUpWindow.gd
@@ -1,11 +1,12 @@
 extends Control
 
 onready var popup = get_node("WindowDialog")
+onready var popup2 = get_node("WindowDialog2")
+onready var popup3 = get_node("WindowDialog3")
 onready var help = get_node("Help")
 onready var tips = get_node("Tips")
 onready var suggestions = get_node("Suggestions")
-onready var popup2 = get_node("WindowDialog2")
-onready var popup3 = get_node("WindowDialog3")
+
 
 func _on_Help_button_up():
 	popup.visible=true
