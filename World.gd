aacec280bbb21d5aaf005eeee7befd87a55c7bee
mkzheng09 <mkzheng@wpi.edu>
2022-04-24 18:13:12 -0400

    Updated ToDoList attached to world home screen
    enter an item, then click to move the item to different lists with the arrows :DDDDDDd

--- World.gd
+++ World.gd
@@ -1,13 +1,10 @@
 extends Node2D
 
-
 func _on_ToDo_pressed():
-	get_tree().change_scene("res://ToDoNoZone.tscn")
-
+	get_tree().change_scene("res://ToDoWithList.tscn")
 
 func _on_Schedule_pressed():
 	get_tree().change_scene("res://Schedule.tscn")
-
 
 func _on_Habits_pressed():
 	get_tree().change_scene("res://HabbitTracker.tscn")
