5ce71bf57a5fc270855ebd452349ed0eca37215f
RachelBuns <102239223+RachelBuns@users.noreply.github.com>
2022-05-11 11:14:36 -0400

    Player Animation Update

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,12 +1,16 @@
 extends KinematicBody2D
 
+# Sets the speed of the character
 const ACCELERATION = 500
 const MAX_SPEED = 150
 const FRICTION = 600
 
+# Input Vector initialized to zero, allows movement and determines direction of animations
 var velocity = Vector2.ZERO
 
+# Sets path to Animation Player node to provide access
 onready var animationPlayer = $AnimationPlayer
+# Sets path to Animation Tree to provide access
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
@@ -17,6 +21,7 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		# Sets the blend position for both the Idle and Run animations
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationState.travel("Run")
